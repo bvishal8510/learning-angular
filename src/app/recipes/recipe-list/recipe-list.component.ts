f7b7cd1866870083b0d921e43140592ca2fddef7
bvishal8510 <baghel.vishal124124@gmail.com>
2018-07-12 22:47:00 +0530

    made outline of shopping list section

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'A simple test recipe', 'http://www.myrecipes.com/recipe/fettuccine-with-olive-oil')
+    new Recipe('Test Recipe', 'A simple test recipe', 'http://www.myrecipes.com/recipe/fettuccine-with-olive-oil'),
+    new Recipe('Test Recipe 2', 'A simple test recipe2', 'http://www.myrecipes.com/recipe/fettuccine-with-olive-oil')
   ];
 
   constructor() { }
